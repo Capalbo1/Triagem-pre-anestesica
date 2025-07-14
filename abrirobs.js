909aa3a6d205e3c3dfcd08571c02d82489ca3369
Gustavo capalbo dos santos <102641571+Capalbo1@users.noreply.github.com>
2025-07-14 18:27:43 -0300

    Revert "teste"
    This reverts commit d0fe6db8be2327d936885fe0d10899916eae0737.

--- abrirobs.js
+++ abrirobs.js
@@ -17,28 +17,3 @@ document.getElementById("reset").addEventListener("click", function () {
     location.reload(); // Recarrega a página atual
 });
 
-
-
-// Seleciona elementos
-const openModalButton = document.getElementById("openModal");
-const modal = document.getElementById("modal");
-const closeModalButton = document.getElementById("closeModal");
-
-// Abre o modal
-openModalButton.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
-
-// Fecha o modal
-closeModalButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-// Fecha o modal clicando fora dele
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
-
-
